2e6be0b0b2c46a9e27e2766480896370ac02a667
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-28 20:27:44 +0200

    Add library of line logos

---
+++ constants/lines.ts
@@ -0,0 +1,52 @@
+import A from '@/assets/lines/A.png';
+import B from '@/assets/lines/B.png';
+import C from '@/assets/lines/C.png';
+import D from '@/assets/lines/D.png';
+import E from '@/assets/lines/E.png';
+import M1 from '@/assets/lines/Métro_1.png';
+import M2 from '@/assets/lines/Métro_2.png';
+import M3 from '@/assets/lines/Métro_3.png';
+import M3bis from '@/assets/lines/Métro_3bis.png';
+import M4 from '@/assets/lines/Métro_4.png';
+import M5 from '@/assets/lines/Métro_5.png';
+import M6 from '@/assets/lines/Métro_6.png';
+import M7 from '@/assets/lines/Métro_7.png';
+import M7bis from '@/assets/lines/Métro_7bis.png';
+import M8 from '@/assets/lines/Métro_8.png';
+import M9 from '@/assets/lines/Métro_9.png';
+import M10 from '@/assets/lines/Métro_10.png';
+import M11 from '@/assets/lines/Métro_11.png';
+import M12 from '@/assets/lines/Métro_12.png';
+import M13 from '@/assets/lines/Métro_13.png';
+import M14 from '@/assets/lines/Métro_14.png';
+import H from '@/assets/lines/H.png';
+import J from '@/assets/lines/J.png';
+import K from '@/assets/lines/K.png';
+import L from '@/assets/lines/L.png';
+import N from '@/assets/lines/N.png';
+import P from '@/assets/lines/P.png';
+import R from '@/assets/lines/R.png';
+import U from '@/assets/lines/U.png';
+import V from '@/assets/lines/V.png';
+import T1 from '@/assets/lines/Tram_1.png';
+import T2 from '@/assets/lines/Tram_2.png';
+import T3a from '@/assets/lines/Tram_3a.png';
+import T3b from '@/assets/lines/Tram_3b.png';
+import T4 from '@/assets/lines/Tram_4.png';
+import T5 from '@/assets/lines/Tram_5.png';
+import T6 from '@/assets/lines/Tram_6.png';
+import T7 from '@/assets/lines/Tram_7.png';
+import T8 from '@/assets/lines/Tram_8.png';
+import T9 from '@/assets/lines/Tram_9.png';
+import T10 from '@/assets/lines/Tram_10.png';
+import T11 from '@/assets/lines/Tram_11.png';
+import T12 from '@/assets/lines/Tram_12.png';
+import T13 from '@/assets/lines/Tram_13.png';
+import T14 from '@/assets/lines/Tram_14.png';
+import C1 from '@/assets/lines/Câble_1.png';
+
+export const lines = {
+    A,B,C,D,E,M1,M2,M3,M3bis,M4,M5,M6,M7,M7bis,M8,M9,M10,M11,M12,M13,M14,H,J,K,L,N,P,R,U,V,T1,T2,T3a,T3b,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,C1
+}
+
+export type LinesType = keyof typeof lines;
